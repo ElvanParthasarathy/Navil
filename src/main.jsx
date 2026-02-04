c3207b7fb9277988a4d0e5ec73e78d007e5dbc07
ElvanParthasarathy <jaiprakashpartha@gmail.com>
2026-02-04 16:45:58 +0530

    Fix Service Worker registration in dev mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+    if (import.meta.env.PROD) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/sw.js')
+                .then(reg => console.log('SW registered!', reg))
+                .catch(err => console.log('SW registration failed!', err));
+        });
+    } else {
+        // Dev mode: unregister to avoid caching issues
+        navigator.serviceWorker.getRegistrations().then(regs => {
+            regs.forEach(reg => {
+                console.log('Unregistering SW for dev mode:', reg);
+                reg.unregister();
+            });
+        });
+    }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
