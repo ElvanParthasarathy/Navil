b553a2db6c6e07a5e51b051727ca5b4e9d508df3
ElvanParthasarathy <jaiprakashpartha@gmail.com>
2026-05-14 00:37:20 +0530

    Move Firebase config to env variables

--- src/lib/firebaseClient.js
+++ src/lib/firebaseClient.js
@@ -3,14 +3,14 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCA__Mtb9oNy_NnglGfn3WDkb47dbbi8tI",
-  authDomain: "elvanparthasarathy.firebaseapp.com",
-  databaseURL: "https://elvanparthasarathy-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "elvanparthasarathy",
-  storageBucket: "elvanparthasarathy.firebasestorage.app",
-  messagingSenderId: "388935758553",
-  appId: "1:388935758553:web:9da2868cd8c03d937dc4c8",
-  measurementId: "G-TYDPNQ7Z8S"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
