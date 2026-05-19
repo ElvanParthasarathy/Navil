780effd9fd51cdc66ee8a2e87e32ef13f78dfb60
ElvanParthasarathy <jaiprakashpartha@gmail.com>
2026-05-20 01:55:29 +0530

    Use circular-cropped SVG favicon with CDN profile image

--- public/sw.js
+++ public/sw.js
@@ -3,6 +3,7 @@ const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
     '/manifest.webmanifest',
+    '/favicon.svg',
     '/assets/instagram/profile.jpg',
     '/assets/style.css',
     '/assets/mobile.css'
